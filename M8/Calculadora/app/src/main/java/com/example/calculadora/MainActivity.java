8e1d08562fb5a55b591108b6d0765a25cea8e839
dgr-85 <davidgomezrovira85@gmail.com>
2023-09-22 19:03:14 +0200

    Divendres 22/9 M8

--- M8/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ M8/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etVal1;
+    private EditText etVal2;
+    private TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etVal1 = findViewById(R.id.etValue1);
+        etVal2 = findViewById(R.id.etValue2);
+        tvResult = findViewById(R.id.tvResult);
     }
+
+    public void bAddOnClick(View view) {
+        int val1 = Integer.parseInt(etVal1.getText().toString());
+        int val2 = Integer.parseInt(etVal2.getText().toString());
+
+        int sum = val1 + val2;
+        tvResult.setText(String.format(new Locale("es", "ES"),"%,d", sum));
+    }
+
 }
