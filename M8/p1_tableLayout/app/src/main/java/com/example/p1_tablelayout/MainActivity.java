fb03997006939143714c88dcf16205978232a3fa
dgr-85 <davidgomezrovira85@gmail.com>
2023-10-08 23:09:18 +0200

    M8 TableLayout

---
+++ M8/p1_tableLayout/app/src/main/java/com/example/p1_tablelayout/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.p1_tablelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
