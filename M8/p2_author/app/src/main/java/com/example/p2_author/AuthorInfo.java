b0f709882939e624374f00babbea8471a613dbb3
dgr-85 <davidgomezrovira85@gmail.com>
2023-10-17 19:30:24 +0200

    Dimarts 17/10 M8 Second Activity

--- M8/p2_author/app/src/main/java/com/example/p2_author/AuthorInfo.java
+++ M8/p2_author/app/src/main/java/com/example/p2_author/AuthorInfo.java
@@ -3,12 +3,25 @@ package com.example.p2_author;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AuthorInfo extends AppCompatActivity {
+
+    Button btnClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_author_info);
+
+        btnClose=findViewById(R.id.infoButton);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
     }
 }
--- M8/p2_author/app/src/main/java/com/example/p2_author/MainActivity.java
+++ M8/p2_author/app/src/main/java/com/example/p2_author/MainActivity.java
@@ -2,18 +2,30 @@ package com.example.p2_author;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnAuthor=findViewById(R.id.btnAuthor);
+    Button btnAuthor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnAuthor=findViewById(R.id.btnAuthor);
+        btnAuthor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                on_BtnAuthorClick();
+            }
+        });
+    }
 
+    public void on_BtnAuthorClick(){
+        startActivity(new Intent(this, AuthorInfo.class));
     }
 }
