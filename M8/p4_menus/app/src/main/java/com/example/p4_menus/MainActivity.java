a7b5ca0b966910cfcd4b8322544a376c933d779c
dgr-85 <davidgomezrovira85@gmail.com>
2023-11-10 19:01:31 +0100

    Divendres 10/11 M8 p4_menus

--- M8/p4_menus/app/src/main/java/com/example/p4_menus/MainActivity.java
+++ M8/p4_menus/app/src/main/java/com/example/p4_menus/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.p4_menus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Button btnHide=findViewById(R.id.btnHideToolbar);
+        Button btnShow=findViewById(R.id.btnShowToolbar);
+
+        btnHide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportActionBar().hide();
+            }
+        });
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportActionBar().show();
+            }
+        });
+
     }
 }
