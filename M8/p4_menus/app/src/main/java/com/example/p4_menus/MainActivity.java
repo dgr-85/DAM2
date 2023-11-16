9c216bac2014262881d03f0e6aaf54567672e51d
dgr-85 <davidgomezrovira85@gmail.com>
2023-11-16 20:12:07 +0100

    Dijous 16/11 M10 apps tercers i M8 menus (vaga)

--- M8/p4_menus/app/src/main/java/com/example/p4_menus/MainActivity.java
+++ M8/p4_menus/app/src/main/java/com/example/p4_menus/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.p4_menus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,28 +21,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Button btnHide=findViewById(R.id.btnHideToolbar);
         Button btnShow=findViewById(R.id.btnShowToolbar);
         btnShow.setEnabled(false);
 
-        btnHide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getSupportActionBar().hide();
+        btnHide.setOnClickListener((view) -> {
+                Objects.requireNonNull(getSupportActionBar()).hide();
                 btnHide.setEnabled(false);
                 btnShow.setEnabled(true);
-            }
         });
 
-        btnShow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getSupportActionBar().show();
+        btnShow.setOnClickListener((view)-> {
+                Objects.requireNonNull(getSupportActionBar()).show();
                 btnHide.setEnabled(true);
                 btnShow.setEnabled(false);
-            }
         });
     }
     @Override
@@ -57,6 +54,12 @@ public class MainActivity extends AppCompatActivity {
         } else if (item.getItemId()==R.id.action_settings) {
             String option=getString(R.string.option_selected)+" "+getString(R.string.action_settings);
             Toast.makeText(this, option, Toast.LENGTH_SHORT).show();
+            return true;
+        }else if (item.getItemId()==R.id.action_home){
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.institutmarianao.cat/ca/")));
+            return true;
+        }else if(item.getItemId()==R.id.action_code){
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.codewars.com/")));
             return true;
         }else{
             return super.onOptionsItemSelected(item);
