6e8640ea91126f3cb7e9dd9883300e3bbdcded66
dgr-85 <davidgomezrovira85@gmail.com>
2023-12-15 18:59:52 +0100

    Divendres 15/12 M6 hibernate i M8 p6 i p7

--- M8/p6_fitxers/app/src/main/java/com/example/p6_fitxers/MainActivity.java
+++ M8/p6_fitxers/app/src/main/java/com/example/p6_fitxers/MainActivity.java
@@ -3,6 +3,15 @@ package com.example.p6_fitxers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        readFile();
+    }
+
+    public void readFile(){
+        try {
+            InputStream streamRaw = getResources().openRawResource(R.raw.datos_coches);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(streamRaw));
+            String str;
+            List<String> series=new ArrayList<>();
+            Map<String,List<String>> cars = new HashMap<>();
+            while(!((str = reader.readLine()).equals("end"))){
+                series.add(str);
+            }
+            while((str=reader.readLine())!=null){
+
+            }
+            streamRaw.close();
+        } catch (Exception ex) {
+            Log.e("Files", " Error reading file from raw resource.");
+        }
     }
 }
