19526624a6f838a91bc98fc78fa1d98ca9dbc73b
dgr-85 <davidgomezrovira85@gmail.com>
2024-01-12 00:57:59 +0100

    M7 calculadora

--- M7/JavaFX/p10_DGR_calculadora/src/application/SampleController.java
+++ M7/JavaFX/p10_DGR_calculadora/src/application/SampleController.java
@@ -1,5 +1,34 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class SampleController {
 	
+	@FXML
+	private AnchorPane apGenere;
+	
+	@FXML
+	private Slider sAltura;
+	
+	@FXML
+	private DatePicker dpNaixement;
+	
+	@FXML
+	private TextField tfPes;
+	
+	@FXML
+	private TextField tfTMB;
+	
+	@FXML
+	private Button btnCalcular;
+	
+	@FXML
+	private void initialize() {
+		
+	}
 }
