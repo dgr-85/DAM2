bbc26e972c9e85601152d37e7cf2b56c5e56dc0c
dgr-85 <davidgomezrovira85@gmail.com>
2024-01-16 15:48:54 +0100

    M7 calculadora acabada

--- M7/JavaFX/p10_DGR_calculadora/src/application/SampleController.java
+++ M7/JavaFX/p10_DGR_calculadora/src/application/SampleController.java
@@ -1,7 +1,12 @@
 package application;
 
-import javafx.event.ActionEvent;
+import java.time.LocalDate;
+import java.time.Period;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -43,34 +48,67 @@ public class SampleController {
 	private void initialize() {
 
 		rbDona.setSelected(true);
+		tfTMB.setText("0");
+		dpNaixement.setValue(LocalDate.now().minus(Period.ofYears(20)));
 
 		sAltura.setMin(150);
 		sAltura.setMax(200);
 		sAltura.setValue(165);
+		sAltura.valueProperty().addListener(new ChangeListener<Number>() {
 
-//		sAltura.valueProperty().addListener(new ChangeListener<Number>() {
-//
-//			@Override
-//			public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
-//				valorAltura.setText(String.format("%d cm", arg2));
-//
-//			}
-//		});
+			@Override
+			public void changed(ObservableValue<? extends Number> arg0, Number arg1, Number arg2) {
+				valorAltura.setText(String.format("%d cm", arg2.intValue()));
+
+			}
+		});
 
 	}
 
 	@FXML
-	void calcularResultat(ActionEvent event) {
+	Integer calcularData(LocalDate dataEscollida) {
+		Period periode = Period.between(dataEscollida, LocalDate.now());
+		int edat = periode.getYears();
+		if (edat < 20 || edat > 70) {
+			mostrarAlerta("Edat no vàlida", "L'edat ha d'estar compresa entre 20 i 70 anys, ambós inclosos.");
+			return null;
+		}
+		return edat;
+	}
+
+	@FXML
+	Integer calcularPes(String pesEscollit) {
+		int pes = Integer.parseInt(pesEscollit);
+		if (pes < 25 || pes > 125) {
+			mostrarAlerta("Pes invàlid", "El pes ha de tenir un valor entre 25 i 125, ambdós inclosos.");
+			return null;
+		}
+		return pes;
+	}
+
+	@FXML
+	void calcularResultat() {
+		Integer pes = calcularPes(tfPes.getText());
+		Integer edat = calcularData(dpNaixement.getValue());
 		int sexe;
 		if (rbDona.isSelected()) {
 			sexe = -161;
 		} else {
 			sexe = 5;
 		}
-		int pes = Integer.parseInt(tfPes.getText());
 		int altura = (int) sAltura.getValue();
 
-		int res = (int) (pes * 10 + 6.25 * altura - 100 + sexe);
-		tfTMB.setText(String.valueOf(res));
+		if (pes != null && edat != null) {
+			int res = (int) Math.round(pes * 10 + 6.25 * altura - edat * 5 + sexe);
+			tfTMB.setText(String.valueOf(res));
+		}
+	}
+
+	@FXML
+	void mostrarAlerta(String titol, String text) {
+		Alert alert = new Alert(Alert.AlertType.WARNING);
+		alert.setTitle(titol);
+		alert.setContentText(text);
+		alert.showAndWait();
 	}
 }
--- M7/JavaFX/p10_DGR_calculadora/src/module-info.java
+++ M7/JavaFX/p10_DGR_calculadora/src/module-info.java
@@ -1,6 +1,7 @@
 module p10_DGR_calculadora {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
