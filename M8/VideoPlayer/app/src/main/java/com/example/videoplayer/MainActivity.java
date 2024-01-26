5e6b0b55aea971d872ed4fe25fdcf0276e7bbebf
dgr-85 <davidgomezrovira85@gmail.com>
2024-01-26 19:34:15 +0100

    Divendres 26/1 M6 hibernate i M8 video player

--- M8/VideoPlayer/app/src/main/java/com/example/videoplayer/MainActivity.java
+++ M8/VideoPlayer/app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -2,18 +2,44 @@ package com.example.videoplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.Uri;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView image;
+    private Button btnPlay;
+    private TextView tvPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnPlay=findViewById(R.id.btnPlay);
+        tvPlay=findViewById(R.id.tvPlay);
+
+        MediaPlayer mediaPlayer=MediaPlayer.create(MainActivity.this,R.raw.letitbe);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.reset();
+                }else{
+                    mediaPlayer.start();
+                }
+                tvPlay.setText("Playing...");
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        tvPlay.setText("Finished.");
+                    }
+                });
+            }
+        });
     }
+
 }
--- M8/VideoPlayer/app/src/main/java/com/example/videoplayer/ReproductorActivity.java
+++ M8/VideoPlayer/app/src/main/java/com/example/videoplayer/ReproductorActivity.java
@@ -2,7 +2,6 @@ package com.example.videoplayer;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,8 +14,8 @@ public class ReproductorActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        botonReproducir = findViewById(R.id.button1);
-        lbEstado = findViewById(R.id.textView1);
+        botonReproducir = findViewById(R.id.btnPlay);
+        lbEstado = findViewById(R.id.tvPlay);
 //También puedes asociar la propiedad Onclic del botón con el método onClick en vez de utilizar OnClickListener
 //botonReproducir.setOnClickListener(new
 //View.OnClickListener() {
