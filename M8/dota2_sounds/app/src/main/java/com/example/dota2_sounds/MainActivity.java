3c7e0273de660d58ef9350847e147069440767bc
dgr-85 <davidgomezrovira85@gmail.com>
2024-02-09 18:59:57 +0100

    Divendres 9/2 M6 propietaris i M8 dota2_sounds

--- M8/dota2_sounds/app/src/main/java/com/example/dota2_sounds/MainActivity.java
+++ M8/dota2_sounds/app/src/main/java/com/example/dota2_sounds/MainActivity.java
@@ -2,28 +2,52 @@ package com.example.dota2_sounds;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Integer> selectedElements = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnPlay=findViewById(R.id.btnPlay);
-        MediaPlayer mp=MediaPlayer.create(MainActivity.this,R.raw.arena_of_blood);
+        Button btnPlay = findViewById(R.id.btnPlay);
+        MediaPlayer mp = MediaPlayer.create(MainActivity.this, R.raw.primal_roar);
+
+
+        int soundsLength = R.raw.class.getFields().length;
+
+        TextView tv1 = findViewById(R.id.tv1);
+        TextView tv2 = findViewById(R.id.tv2);
+        TextView tv3 = findViewById(R.id.tv3);
+
+        ImageView img1 = findViewById(R.id.img1);
+        ImageView img2 = findViewById(R.id.img2);
+        ImageView img3 = findViewById(R.id.img3);
+
+        ImageView[] imgs = {img1, img2, img3};
+        TextView[] texts = {tv1, tv2, tv3};
+
+        newRound(imgs, texts);
+
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mp.isPlaying()){
+                /*if(mp.isPlaying()){
                     mp.stop();
                     try {
                         mp.prepare();
@@ -32,8 +56,38 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }else{
                     mp.start();
-                }
+                }*/
+                newRound(imgs, texts);
             }
         });
     }
+
+    public void newRound(ImageView[] imgs, TextView[] texts) {
+
+        Field[] listElements = R.raw.class.getFields();
+        int totalElements = listElements.length;
+        Field[] listImages = R.drawable.class.getFields();
+        int randomNumber = (int) (Math.random() * totalElements);
+
+        for (int i = 0; i < imgs.length; i++) {
+            while (selectedElements.contains(randomNumber)) {
+                randomNumber = (int) (Math.random() * totalElements);
+            }
+            if (selectedElements.size() < 3) {
+                selectedElements.add(randomNumber);
+            } else {
+                selectedElements.set(i, randomNumber);
+            }
+
+            Field numberElem = listElements[randomNumber];
+            String elemName = numberElem.getName();
+
+            int id = getResources().getIdentifier(elemName, "string", getPackageName());
+            String str = String.valueOf(getString(id));
+            texts[i].setText(str);
+
+            imgs[i].setImageDrawable(getDrawable(getResources().getIdentifier(elemName, "drawable", getPackageName())));
+        }
+    }
+
 }
