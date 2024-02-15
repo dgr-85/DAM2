8d9c920b19953dac99acdc60b85ebff5e210dcc4
dgr-85 <davidgomezrovira85@gmail.com>
2024-02-15 21:12:47 +0100

    Dijous 15/2 M7 penjat

--- M7/JavaFX/javafx_framework/src/application/Vista1Controller.java
+++ M7/JavaFX/javafx_framework/src/application/Vista1Controller.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -12,10 +13,35 @@ public class Vista1Controller {
 	@FXML
 	private ImageView cardImg;
 
-	Image img = new Image(getClass().getResourceAsStream("imgs/carta_12.jpg"));
+	@FXML
+	private Button btnPlay;
+
+	Image[] images = new Image[13];
+	String[] hiddenWords = { "AGNÒSTIQUES", "ESCURÇÓ", "FRUÏCIÓ", "GALVANOTÈCNIA", "PASTANAGA", "MASSÍS" };
+
+	boolean loaded = false;
+	int count = 12;
+
+	public void instantiateImages() {
+		for (int i = 0; i < 13; i++) {
+			if (i < 10) {
+				images[i] = new Image(getClass().getResourceAsStream("resource/carta_0" + i + ".jpg"));
+			} else {
+				images[i] = new Image(getClass().getResourceAsStream("resource/carta_" + i + ".jpg"));
+			}
+		}
+	}
 
 	public void loadCard() {
-		cardImg.setImage(img);
+		if (!loaded) {
+			instantiateImages();
+			loaded = true;
+		}
+		cardImg.setImage(images[count--]);
+		if (count < 0) {
+			count = 12;
+		}
+
 	}
 
 }
