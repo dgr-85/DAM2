3da988682b68e34b22318906d1616789e01d1517
dgr-85 <davidgomezrovira85@gmail.com>
2024-02-23 14:23:57 +0100

    M8 musicPlayer

--- M8/MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
+++ M8/MusicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.musicplayer;
 
+import android.Manifest;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_MEDIA_AUDIO)) {
+                // permission was granted, yay! Do the external storage task you need to do.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_AUDIO}, MY_PERMISSIONS_REQUEST);
+                // MY_PERMISSIONS_REQUEST is an app-defined int constant. The callback method gets the result of the request.
+            }
+        } else {
+            updateSongList();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        boolean mediaAudioPermission = false;
+        boolean externalStoragePermission = false;
+
+        if (requestCode == MY_PERMISSIONS_REQUEST) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) { // permission was granted, yay! Do the external storage task you need to do.
+                updateSongList();
+            }
+        }
+    }
+
+    public void updateSongList(){
+
     }
 }
