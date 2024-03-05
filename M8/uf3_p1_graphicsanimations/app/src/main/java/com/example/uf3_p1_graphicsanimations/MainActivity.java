86a6f3e6ffa794ca4e89c499bed848a7e600b12a
dgr-85 <davidgomezrovira85@gmail.com>
2024-03-05 21:04:52 +0100

    Dimarts 5/3 M10 gimnàs, M6 exempleMongo i M8 animacions

---
+++ M8/uf3_p1_graphicsanimations/app/src/main/java/com/example/uf3_p1_graphicsanimations/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.uf3_p1_graphicsanimations;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         ConstraintLayout main= findViewById(R.id.cLayout);
+         main.setOnTouchListener(new View.OnTouchListener() {
+             @Override
+             public boolean onTouch(View v, MotionEvent event) {
+
+                 return false;
+             }
+         });
+    }
+}
