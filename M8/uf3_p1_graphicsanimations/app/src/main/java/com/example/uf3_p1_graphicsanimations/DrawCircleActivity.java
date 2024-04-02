0d4d1dba7345c3eb48439abca3a15031595d2846
dgr-85 <davidgomezrovira85@gmail.com>
2024-04-02 15:45:37 +0200

    M8 gràfics

--- M8/uf3_p1_graphicsanimations/app/src/main/java/com/example/uf3_p1_graphicsanimations/DrawCircleActivity.java
+++ M8/uf3_p1_graphicsanimations/app/src/main/java/com/example/uf3_p1_graphicsanimations/DrawCircleActivity.java
@@ -1,4 +1,62 @@
 package com.example.uf3_p1_graphicsanimations;
 
-public class DrawCircleActivity {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class DrawCircleActivity extends AppCompatActivity implements View.OnTouchListener {
+    private int coordX, coordY;
+    private boolean hasCanvasBeenClicked = false;
+    private CanvasBG canvasBG;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout cLayout = findViewById(R.id.cLayout);
+        canvasBG = new CanvasBG(this);
+        canvasBG.setOnTouchListener(this);
+        cLayout.addView(canvasBG);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        hasCanvasBeenClicked = true;
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            coordX = (int) event.getX();
+            coordY = (int) event.getY();
+            canvasBG.invalidate();
+        }
+        return false;
+    }
+
+    class CanvasBG extends View {
+
+        private Paint brush;
+
+        public CanvasBG(Context context) {
+            super(context);
+            brush = new Paint();
+            brush.setARGB(255, 155, 0, 75);
+            brush.setStrokeWidth(10);
+            brush.setStyle(Paint.Style.STROKE);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawRGB(255, 255, 255);
+            if (hasCanvasBeenClicked) {
+                canvas.drawCircle(coordX, coordY, 100, brush);
+            }
+            hasCanvasBeenClicked = false;
+        }
+    }
+
 }
--- M8/uf3_p1_graphicsanimations/app/src/main/java/com/example/uf3_p1_graphicsanimations/MainActivity.java
+++ M8/uf3_p1_graphicsanimations/app/src/main/java/com/example/uf3_p1_graphicsanimations/MainActivity.java
@@ -10,54 +10,11 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
-
-    private int coordX, coordY;
-    private boolean hasCanvasBeenClicked = false;
-    private CanvasBG canvasBG;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ConstraintLayout cLayout = findViewById(R.id.cLayout);
-        canvasBG = new CanvasBG(this);
-        canvasBG.setOnTouchListener(this);
-        cLayout.addView(canvasBG);
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        hasCanvasBeenClicked = true;
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            coordX = (int) event.getX();
-            coordY = (int) event.getY();
-            canvasBG.invalidate();
-        }
-        return false;
-    }
-
-    class CanvasBG extends View {
-
-        private Paint brush;
-
-        public CanvasBG(Context context) {
-            super(context);
-            brush = new Paint();
-            brush.setARGB(255, 155, 0, 75);
-            brush.setStrokeWidth(10);
-            brush.setStyle(Paint.Style.STROKE);
-        }
-
+public class MainActivity extends AppCompatActivity{
         @Override
-        protected void onDraw(Canvas canvas) {
-            canvas.drawRGB(255, 255, 255);
-            if (hasCanvasBeenClicked) {
-                canvas.drawCircle(coordX, coordY, 100, brush);
-            }
-            hasCanvasBeenClicked = false;
-        }
-    }
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
+        }
 }
