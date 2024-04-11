154ed4b5ee37c1aea78fefa22d0808631e08de3e
dgr-85 <davidgomezrovira85@gmail.com>
2024-04-11 21:11:26 +0200

    Dijous 11/4 M7 xifrat i M10 consulta guardada

---
+++ M7/Python/ProvaQt/PyQt_03.py
@@ -0,0 +1,15 @@
+def codif(txt, clau):
+    alfabet = 'abcdefghijklmnopqrstuvwxyz'
+    txt_cod = ''
+    for lletra in txt:
+        if lletra.lower() in alfabet:
+            lletra_nova = alfabet[(alfabet.index(lletra.lower()) + clau) % len(alfabet)]
+            if lletra.isupper():
+                lletra_nova = lletra_nova.upper()
+            txt_cod = txt_cod + lletra_nova
+        else:
+            txt_cod = txt_cod + lletra
+    return txt_cod
+
+
+print(codif('Comprimint línees de codi... Èxit!', 3))
