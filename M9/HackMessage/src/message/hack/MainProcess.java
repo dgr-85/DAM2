83d8b40b4a507d7462dc5c218db74fa662db78d7
dgr-85 <davidgomezrovira85@gmail.com>
2024-04-24 21:15:03 +0200

    Dimecres 24/4 M9 hack

--- M9/HackMessage/src/message/hack/MainProcess.java
+++ M9/HackMessage/src/message/hack/MainProcess.java
@@ -1,60 +1,64 @@
 package message.hack;
 
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.KeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Arrays;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class MainProcess {
 
 	static byte[] decryptedData = null;
+	static String decryptedString = null;
 
 	public static void main(String[] args) {
-		int[] encodedMessage = { 0xB3, 0xF9, 0xB2, 0x72, 0xA9, 0x07, 0x1F, 0xA4, 0x91, 0xA0, 0x1B, 0x48, 0x0D, 0x4F,
-				0xEC, 0xA0, 0x56, 0x54, 0x4A, 0xBB, 0x3C, 0xF1, 0xFF, 0x4F, 0xD0, 0x7F, 0xA6, 0x9B, 0x8F, 0x74, 0xF6,
-				0x8F };
+		byte[] encodedMessage = { (byte) 0xB3, (byte) 0xF9, (byte) 0xB2, 0x72, (byte) 0xA9, 0x07, 0x1F, (byte) 0xA4,
+				(byte) 0x91, (byte) 0xA0, 0x1B, 0x48, 0x0D, 0x4F, (byte) 0xEC, (byte) 0xA0, 0x56, 0x54, 0x4A,
+				(byte) 0xBB, 0x3C, (byte) 0xF1, (byte) 0xFF, 0x4F, (byte) 0xD0, 0x7F, (byte) 0xA6, (byte) 0x9B,
+				(byte) 0x8F, 0x74, (byte) 0xF6, (byte) 0x8F };
 		int password = 0;
-		String salt = "128";
+		int size = 256;
 		Boolean eureka = false;
+		Boolean itWorks = false;
 
-		System.out.println("Searching...");
-		while (!eureka && password < 99999) {
-			SecretKey sKey = generateSecretKey(String.format("%05d", password), salt);
-			eureka = decryptData(sKey, encodedMessage.getBytes());
+		System.out.print("Searching");
+		while (!eureka && password <= 99999 && !itWorks) {
+			SecretKey sKey = generateSecretKey(String.format("%05d", password), size);
+			eureka = decryptData(sKey, encodedMessage);
+
 			password++;
-			// System.out.println(String.format("%05d", password));
-		}
 
-		try {
-			if (password == 99999) {
-				System.out.println("99999 has been reached. Code cracking has failed.");
-			} else {
-				System.out.println(new String(decryptedData, "UTF-8"));
+			itWorks = (decryptedString != null && decryptedString.matches("[\\p{Print}]+"));
+
+			if (password % 10 == 0) {
+				System.out.print(".");
 			}
-
-		} catch (UnsupportedEncodingException e) {
-			System.out.println("Error transforming bytes to string.");
+		}
+		if (password > 99999) {
+			System.out.println(
+					System.lineSeparator() + "All possible passwords have proven wrong. Code cracking has failed.");
+		} else {
+			System.out.println(System.lineSeparator() + String.format("%05d", password));
+			System.out.println(decryptedString);
 		}
 	}
 
-	public static SecretKey generateSecretKey(String password, String salt) {
-		try {
-			SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-			KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
-			return new SecretKeySpec(keyFactory.generateSecret(spec).getEncoded(), "AES");
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			return null;
-		} catch (InvalidKeySpecException e) {
-			e.printStackTrace();
-			return null;
+	public static SecretKey generateSecretKey(String password, int size) {
+		SecretKey sKey = null;
+		if ((size == 128) || (size == 192) || (size == 256)) {
+			try {
+				byte[] data = password.getBytes("UTF-8");
+				MessageDigest md = MessageDigest.getInstance("SHA-256");
+				byte[] hash = md.digest(data);
+				byte[] key = Arrays.copyOf(hash, size / 8);
+				sKey = new SecretKeySpec(key, "AES");
+			} catch (Exception ex) {
+				System.err.println("Could not generate the AES key:" + ex);
+			}
 		}
+		return sKey;
 	}
 
 	public static Boolean decryptData(SecretKey key, byte[] data) {
@@ -62,11 +66,10 @@ public class MainProcess {
 			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
 			cipher.init(Cipher.DECRYPT_MODE, key);
 			decryptedData = cipher.doFinal(data);
+			decryptedString = new String(decryptedData, StandardCharsets.UTF_8);
 			return true;
 		} catch (Exception ex) {
-			// System.err.println("Could not decrypt the data: " + ex);
 			return false;
 		}
-
 	}
 }
