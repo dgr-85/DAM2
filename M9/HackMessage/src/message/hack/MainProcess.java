8ce952e003d59db85fdb0005de33ea92cd8c18c2
dgr-85 <davidgomezrovira85@gmail.com>
2024-04-27 17:24:41 +0200

    M9 hack acabat

--- M9/HackMessage/src/message/hack/MainProcess.java
+++ M9/HackMessage/src/message/hack/MainProcess.java
@@ -18,30 +18,26 @@ public class MainProcess {
 				(byte) 0x91, (byte) 0xA0, 0x1B, 0x48, 0x0D, 0x4F, (byte) 0xEC, (byte) 0xA0, 0x56, 0x54, 0x4A,
 				(byte) 0xBB, 0x3C, (byte) 0xF1, (byte) 0xFF, 0x4F, (byte) 0xD0, 0x7F, (byte) 0xA6, (byte) 0x9B,
 				(byte) 0x8F, 0x74, (byte) 0xF6, (byte) 0x8F };
-		int password = 0;
-		int size = 256;
-		Boolean eureka = false;
-		Boolean itWorks = false;
+		Integer password = 0;
+		Integer size = 256;
+		Boolean resultIsUTF8Encoded = false;
 
 		System.out.print("Searching");
-		while (!eureka && password <= 99999 && !itWorks) {
+		while (password <= 99999 && !resultIsUTF8Encoded) {
 			SecretKey sKey = generateSecretKey(String.format("%05d", password), size);
-			eureka = decryptData(sKey, encodedMessage);
-
-			password++;
-
-			itWorks = (decryptedString != null && decryptedString.matches("[\\p{Print}]+"));
-
-			if (password % 10 == 0) {
+			decryptData(sKey, encodedMessage);
+			if (!(resultIsUTF8Encoded = (decryptedString != null && decryptedString.matches("[\\p{Print}]+"))))
+				password++;
+			if (password % 500 == 0) {
 				System.out.print(".");
 			}
 		}
 		if (password > 99999) {
-			System.out.println(
-					System.lineSeparator() + "All possible passwords have proven wrong. Code cracking has failed.");
+			System.out.println(System.lineSeparator()
+					+ "All possible passwords have proven ineffective. Code cracking has failed.");
 		} else {
-			System.out.println(System.lineSeparator() + String.format("%05d", password));
-			System.out.println(decryptedString);
+			System.out.println(System.lineSeparator() + "Password found: " + String.format("%05d", password));
+			System.out.println("Message decrypted: " + decryptedString);
 		}
 	}
 
@@ -55,21 +51,19 @@ public class MainProcess {
 				byte[] key = Arrays.copyOf(hash, size / 8);
 				sKey = new SecretKeySpec(key, "AES");
 			} catch (Exception ex) {
-				System.err.println("Could not generate the AES key:" + ex);
+				System.err.println("Error while generating AES key:" + ex);
 			}
 		}
 		return sKey;
 	}
 
-	public static Boolean decryptData(SecretKey key, byte[] data) {
+	public static void decryptData(SecretKey key, byte[] data) {
 		try {
 			Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
 			cipher.init(Cipher.DECRYPT_MODE, key);
 			decryptedData = cipher.doFinal(data);
 			decryptedString = new String(decryptedData, StandardCharsets.UTF_8);
-			return true;
 		} catch (Exception ex) {
-			return false;
 		}
 	}
 }
